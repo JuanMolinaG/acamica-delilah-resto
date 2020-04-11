90f4bcdc45d0dca8714a90993d628e49578d0404
Juan G. Molina <jg.molina92@hotmail.com>
2020-04-11 17:36:04 -0500

    Add cors module

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const Token = require( './helpers/jwtGenerator' );
+const cors = require( 'cors' );
 
 const userToken = new Token();
 
 const app = express();
 app.use( bodyParser.json() );
+app.use( cors() );
 
 app.get( '/v1/users', ( req, res ) => {
     res.send( 'Hola desde Express' );
