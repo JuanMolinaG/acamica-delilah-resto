3fbafb0f97d15d6137478a225e29ec428fe1763a
Juan G. Molina <jg.molina92@hotmail.com>
2020-04-12 16:34:24 -0500

    Move jwt helper files to own folder

---
+++ server/helpers/jwt/jwtSecretKey.js
@@ -0,0 +1,3 @@
+const secret_key = 'b4s1c4p1s3cr3tk31';
+
+module.exports = secret_key;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
-const Token = require( './helpers/jwtGenerator' );
+const Token = require( './helpers/jwt/jwtGenerator' );
 const cors = require( 'cors' );
 
 const userToken = new Token();
