2177e19f2aae1f03f584707766471aa32975131c
Juan G. Molina <jg.molina92@hotmail.com>
2020-08-08 13:32:49 -0500

    Add swagger-ui-express module and documentation endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
+const SwaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./spec.json');
 
 // Import Routes
 const usersRoutes = require('./routes/users.route');
@@ -17,6 +19,7 @@ app.use(cors());
 app.use('/api/users', usersRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/orders', ordersRoutes);
+app.use('/api/docs', SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
